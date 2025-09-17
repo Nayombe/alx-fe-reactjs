0893d65bc5f3340d94ea6e8078d5055a8eea2edb
Brian Nayombe <133241883+Nayombe@users.noreply.github.com>
2025-09-17 19:44:57 +0300

    Update Footer.jsx

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,1 +1,11 @@
-ECHO is on.
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: '#222', color: 'white', textAlign: 'center', padding: '15px', marginTop: '20px' }}>
+      <p>&copy; 2025 My Favorite Cities. All Rights Reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
