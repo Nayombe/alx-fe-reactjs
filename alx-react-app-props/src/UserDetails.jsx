317ff7e0002cadf2c5cb61c61e35e06c7c0b30af
Brian Nayombe <133241883+Nayombe@users.noreply.github.com>
2025-09-18 09:06:58 +0300

    Update UserDetails.jsx

--- alx-react-app-props/src/UserDetails.jsx
+++ alx-react-app-props/src/UserDetails.jsx
@@ -1,1 +1,15 @@
-ECHO is on.
+import React, { useContext } from "react";
+import UserContext from "./UserContext";
+
+function UserDetails() {
+  const userData = useContext(UserContext);
+
+  return (
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
+}
+
+export default UserDetails;
