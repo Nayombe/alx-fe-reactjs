4f5ee98427a6b6d5a29ee3a85a262eb1d3131895
Brian Nayombe <133241883+Nayombe@users.noreply.github.com>
2025-09-20 11:32:39 +0300

    Update Services.jsx

--- my-company/src/Services.jsx
+++ my-company/src/Services.jsx
@@ -1,1 +1,14 @@
-ECHO is on.
+function Services() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Our Services</h1>
+      <ul>
+        <li>Technology Consulting</li>
+        <li>Market Analysis</li>
+        <li>Product Development</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Services;
