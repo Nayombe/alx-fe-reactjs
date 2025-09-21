88a43b88f7264567d82d12f3fd815ccd220a55ff
Brian Nayombe <133241883+Nayombe@users.noreply.github.com>
2025-09-21 17:39:52 +0300

    Create FavouritesList.jsx

---
+++ recipe-sharing-app/src/components/FavouritesList.jsx
@@ -0,0 +1,27 @@
+import { useRecipeStore } from './recipeStore';
+
+const FavoritesList = () => {
+  const favorites = useRecipeStore((state) =>
+    state.favorites
+      .map((id) => state.recipes.find((recipe) => recipe.id === id))
+      .filter(Boolean) // remove nulls
+  );
+
+  if (favorites.length === 0) {
+    return <p>No favorites yet. Add some recipes!</p>;
+  }
+
+  return (
+    <div>
+      <h2>My Favorites</h2>
+      {favorites.map((recipe) => (
+        <div key={recipe.id} style={{ marginBottom: 12 }}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default FavoritesList;
